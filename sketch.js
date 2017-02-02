adb3999f5dfcc0f4cb65175a94614f2390b00f54
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-02-02 18:44:18 -0500

    Establish the basic structure with p5.js instance mode

--- sketch.js
+++ sketch.js
@@ -1,7 +1,46 @@
-function setup() {
+var looping = true;
+var sketch = new p5(function(p) {
+    p.x = 100;
+    p.y = 100;
+    p.setup = function() {
+        p.canvas = p.createCanvas(200, 200);
+        p.canvas.addClass('one');
+        p.background(51);
+    }
+    p.draw = function() {
+        p.fill(255, 0, 200, 25);
+        p.noStroke();
+        p.ellipse(p.x, p.y, 48, 48);
+        p.x = p.x + p.random(-10, 10);
+        p.y = p.y + p.random(-10, 10);
+    }
+    p.keyPressed = function() {
+        if (p.keyCode === 32) {
+            if (looping) {
+                p.noLoop();
+                looping = false;
+            } else {
+                p.loop();
+                looping = true;
+            }
+        }
+    }
+});
+var geometry = new p5(function(p) {
+    p.x = 100;
+    p.y = 100;
+    p.setup = function() {
+        p.canvas = p.createCanvas(200, 200);
+        p.canvas.addClass('two');
+        // p.background(51);
+    }
+    p.draw = function() {
+        p.clear();
+        p.fill(255, 255, 0, 25);
+        p.noStroke();
+        p.ellipse(p.x, p.y, 48, 48);
+        p.x = p.x + p.random(-10, 10);
+        p.y = p.y + p.random(-10, 10);
 
-}
-
-function draw() {
-  
-}
+    }
+});
