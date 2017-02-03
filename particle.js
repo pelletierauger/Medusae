d039aed5ea4439db3f768c967e6eb30fa9889880
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-02-02 22:47:01 -0500

    Establish the most basic curvy trail, the same as last year

--- particle.js
+++ particle.js
@@ -1,18 +1,41 @@
 var Particle = function(startX, startY) {
     this.position = new p5.Vector(startX, startY);
-    this.velocity = new p5.Vector(1, 1);
+    this.velocity = new p5.Vector(0.3, 0.3);
     this.history = [];
 }
 
 Particle.prototype.update = function() {
     this.position = p5.Vector.add(this.position, this.velocity);
-    if ((this.position.x > (box.width / 2)) || (this.position.x < -box.width / 2)) {
+    if (this.position.x > (box.width / 2) || this.position.x < -box.width / 2) {
         this.velocity.x *= -1;
     }
 
-    if ((this.position.y > (box.height / 2)) || (this.position.y < -box.height / 2)) {
+    if (this.position.y > (box.height / 2) || this.position.y < -box.height / 2) {
         this.velocity.y *= -1;
     }
+
+    for (var i = 0; i < this.history.length; i++) {
+        this.history[i].x -= sin(this.position.x / 50) * 6.7;
+        this.history[i].y -= sin(this.position.y / 50) * 6.7;
+    }
+
+    var v = new p5.Vector(this.position.x, this.position.y);
+    this.history.push(v);
+    if (this.history.length > 200) {
+        this.history.splice(0, 1);
+    }
 }
+
+Particle.prototype.displayHistory = function() {
+    var size = 1;
+    for (var i = 0; i < (this.history.length - 1); i++) {
+        var pos = this.history[(this.history.length - 1) - i];
+        sketch.push();
+        sketch.translate(pos.x, pos.y);
+        sketch.fill(255, 50);
+        sketch.ellipse(0, 0, size, size);
+        sketch.pop();
+    }
+};
 
 var loneParticle = new Particle(0, 0);
--- sketch.js
+++ sketch.js
@@ -1,36 +1,43 @@
 var looping = true;
 var box = {
-    width: 350,
-    height: 400
+    width: 60,
+    height: 40
 }
 var showGeometry = true;
+var scale = 1;
+var sin = Math.sin;
+var cos = Math.cos;
 
 var sketch = new p5(function(p) {
-    p.x = 200;
-    p.y = 200;
     p.setup = function() {
         p.canvas = p.createCanvas(p.windowWidth, p.windowHeight);
         p.canvas.addClass('one');
-        p.background(51);
+        p.background(0);
         p.noStroke();
+        p.frameRate(30);
     }
     p.draw = function() {
+        // p.background(51);
         p.translate(p.width / 2, p.height / 2);
-        p.fill(255, 0, 0, 50);
-        p.ellipse(loneParticle.position.x, loneParticle.position.y, 5, 5);
+        // p.scale(scale, scale);
+        // p.fill(255, 0, 0, 50);
+        p.ellipse(loneParticle.position.x, loneParticle.position.y, 1, 1);
         loneParticle.update();
+        loneParticle.displayHistory();
     }
     p.keyPressed = function() {
         if (p.keyCode === 32) {
             if (looping) {
                 p.noLoop();
+                geometry.noLoop();
                 looping = false;
             } else {
                 p.loop();
+                geometry.loop();
                 looping = true;
             }
         }
-        if (p.key == 't' || p.key == 'T') {
+        if (p.key == 'g' || p.key == 'G') {
             if (showGeometry) {
                 showGeometry = false;
                 geometry.canvas.style("display", "none");
@@ -38,6 +45,9 @@ var sketch = new p5(function(p) {
                 showGeometry = true;
                 geometry.canvas.style("display", "block");
             }
+        }
+        if (p.key == 'q' || p.key == 'Q') {
+            sketch.background(0);
         }
     }
 });
@@ -47,10 +57,12 @@ var geometry = new p5(function(p) {
     p.setup = function() {
         p.canvas = p.createCanvas(p.windowWidth, p.windowHeight);
         p.canvas.addClass('two');
+        p.frameRate(30);
     }
     p.draw = function() {
         p.clear();
         p.translate(p.width / 2, p.height / 2);
+        // p.scale(scale, scale);
         p.noFill();
         p.stroke(255)
         p.rect(-box.width / 2, -box.height / 2, box.width, box.height);
