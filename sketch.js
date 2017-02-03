c72ce2725d1c8fec76e8f847291e5697acc0d67c
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-02-02 19:24:01 -0500

    Get the basic bouncing particle going, on both canvases

--- sketch.js
+++ sketch.js
@@ -1,18 +1,24 @@
 var looping = true;
+var box = {
+    width: 350,
+    height: 400
+}
+var showGeometry = true;
+
 var sketch = new p5(function(p) {
-    p.x = 100;
-    p.y = 100;
+    p.x = 200;
+    p.y = 200;
     p.setup = function() {
-        p.canvas = p.createCanvas(200, 200);
+        p.canvas = p.createCanvas(p.windowWidth, p.windowHeight);
         p.canvas.addClass('one');
         p.background(51);
+        p.noStroke();
     }
     p.draw = function() {
-        p.fill(255, 0, 200, 25);
-        p.noStroke();
-        p.ellipse(p.x, p.y, 48, 48);
-        p.x = p.x + p.random(-10, 10);
-        p.y = p.y + p.random(-10, 10);
+        p.translate(p.width / 2, p.height / 2);
+        p.fill(255, 0, 0, 50);
+        p.ellipse(loneParticle.position.x, loneParticle.position.y, 5, 5);
+        loneParticle.update();
     }
     p.keyPressed = function() {
         if (p.keyCode === 32) {
@@ -24,23 +30,32 @@ var sketch = new p5(function(p) {
                 looping = true;
             }
         }
+        if (p.key == 't' || p.key == 'T') {
+            if (showGeometry) {
+                showGeometry = false;
+                geometry.canvas.style("display", "none");
+            } else {
+                showGeometry = true;
+                geometry.canvas.style("display", "block");
+            }
+        }
     }
 });
 var geometry = new p5(function(p) {
     p.x = 100;
     p.y = 100;
     p.setup = function() {
-        p.canvas = p.createCanvas(200, 200);
+        p.canvas = p.createCanvas(p.windowWidth, p.windowHeight);
         p.canvas.addClass('two');
-        // p.background(51);
     }
     p.draw = function() {
         p.clear();
-        p.fill(255, 255, 0, 25);
+        p.translate(p.width / 2, p.height / 2);
+        p.noFill();
+        p.stroke(255)
+        p.rect(-box.width / 2, -box.height / 2, box.width, box.height);
+        p.fill(255);
         p.noStroke();
-        p.ellipse(p.x, p.y, 48, 48);
-        p.x = p.x + p.random(-10, 10);
-        p.y = p.y + p.random(-10, 10);
-
+        p.ellipse(loneParticle.position.x, loneParticle.position.y, 5, 5);
     }
 });
