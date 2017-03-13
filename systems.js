f0f6e1e3aa05e437ba9d1b2bb5cad0f1145d6a29
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-03-12 20:16:14 -0400

    Work on loneParticle6, create loneParticle7

--- systems.js
+++ systems.js
@@ -151,17 +151,41 @@ var loneParticle6 = new System({
         var newY = cos(sin(y / 20)) * sin(y / 10) * 6;
         return { x: newX, y: newY };
     },
+    displayFunction: "lines"
+});
+
+var loneParticle7 = new System({
+    startPosition: { x: 0, y: 0 },
+    velocity: { x: 2, y: 2 },
+    box: {
+        width: 50,
+        height: 100
+    },
+    translate: {
+        x: 600,
+        y: 0
+    },
+    scale: {
+        x: 1.2,
+        y: 1.2
+    },
+    historyLength: 210,
+    trailFunction: function(x, y) {
+        var newX = cos(x / 50) * cos(x / 25) * 6;
+        var newY = sin(y / 50) * sin(x / 10) * 6;
+        return { x: newX, y: newY };
+    },
     displayFunction: function(h) {
         sketch.beginShape(sketch.LINES);
         sketch.noFill();
-        sketch.strokeWeight(0.25);
+        sketch.strokeWeight(0.5);
         sketch.stroke(255, 125);
         for (var i = h.length - 1; i >= 1; i--) {
-            sketch.vertex(h[i].x, h[i].y);
-            sketch.vertex(h[i - 1].x, h[i - 1].y);
+            sketch.vertex(h[i].x, h[i].y * 2);
+            sketch.vertex(h[i - 1].x, h[i - 1].y * 2);
         }
         sketch.endShape();
     }
 });
 
-var system = loneParticle3;
+var system = loneParticle7;
